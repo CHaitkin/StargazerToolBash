bfe2d08e8ee0c0b0eed4b012b62a4753a3b5f06e
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-07-12 11:40:00 -0400

    Squashed commit of the following:
    commit c4ee11d0108f6cf0f08c17ab76e791e25bf496b6
    Author: Dee Jae Mann <dee.j.mann@hotmail.com>
    Date:   Fri Jul 12 11:38:56 2024 -0400
    
        Level1 - Added Environment container
    
        Applied ground layer to container
        Implemented updated jump logic
    
    commit 5e0ada94667c77af71a09febb206877e5907a3d8
    Author: Dee Jae Mann <dee.j.mann@hotmail.com>
    Date:   Fri Jul 12 11:35:16 2024 -0400
    
        Updated grounddetection
    
        Added GroundLayer and GroundCheck for GroundDetection logic
    
    commit b3e6d0d1241220278a57eddde506fdf2dad06345
    Merge: 922123b a9ca4db
    Author: Dee Jae Mann <dee.j.mann@hotmail.com>
    Date:   Fri Jul 12 11:04:09 2024 -0400
    
        Merge branch 'Programming' into GroundDetectionFix
    
    commit 922123b66da92fb9c12a93d4a6f9af903ce7191e
    Author: Dee Jae Mann <dee.j.mann@hotmail.com>
    Date:   Fri Jul 12 10:17:02 2024 -0400
    
        Converted original ground collider logic to raycast
    
        Per Muhammad this has a serious flaw when trying to jump from edges of platforms as the detection is not registered. This will be resolved and documented.

--- Assets/Scripts/GroundDetector.cs
+++ Assets/Scripts/GroundDetector.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class GroundDetector : MonoBehaviour
 {
+
+    //This tells us whether we are currently touching the ground
+    //[HideInInspector]
+    //public bool onGround = false;
+
+
+
+    /*
+     * Legacy from provided example template:
+     * 
     // Note to Level 3 Cohort 7: This script is HORRIBLE! ABSOLUTELY ATROCIOUS! And that's on purpose! It's simple and easy to understand.
     // There are several bugs involved with this very simplistic approach to detecting the floor.
     // Try to improve it if you have time! Here's a hint: https://docs.unity3d.com/ScriptReference/Physics2D.Raycast.html
-    
-    //This tells us whether we are currently touching the ground
-    [HideInInspector]
-    public bool onGround = false;
-
+ 
     //If we are touching ANYTHING, we are considered "on the ground"
     public void OnCollisionStay2D(Collision2D collision)
     {
@@ -23,6 +29,24 @@ public class GroundDetector : MonoBehaviour
     {
         onGround = false;
     }
+    */
 
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
+    private void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    public bool IsGrounded()
+    {
+        // Creates 'invisible circle' at players feet
+        // When it collides with the ground layer, we are allowed to jump
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
 }
