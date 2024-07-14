f7cce856567e51b25011c6c468704a5d796fe1e2
Dee Jae Mann <147277261+DeeJaeMann@users.noreply.github.com>
2024-07-14 15:40:35 -0400

    Projectile damage (#7)
    * Added damage to destructible objects
    
    * Fixed projectile collision behavior
    
    * Removed Debug.Log statement

--- Assets/Scripts/ProjectileDestructor.cs
+++ Assets/Scripts/ProjectileDestructor.cs
@@ -6,28 +6,27 @@ using UnityEngine;
 public class ProjectileDestructor : MonoBehaviour
 {
     private GameObject thisProjectile;
+    public int damage = 1;
 
     public float projectileLifespan = 2f;
-    //private float projectileLifeLeft;
 
     // Start is called before the first frame update
     void Start()
     {
         thisProjectile = this.gameObject;
-        //projectileLifeLeft = projectileLifespan;
-        Debug.Log($"Started object {thisProjectile.name}");
         Destroy(thisProjectile, projectileLifespan);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
 
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("I hit something");
+        Destructible destructible = collision.gameObject.GetComponent<Destructible>();
+
+        if (destructible != null)
+        {
+            //Deal damage to the Destructible we touched!
+            destructible.TakeDamage(damage);
+        }
         Die();
     }
 
