a9cb70ff329d5c9b16acb119f4500c1448daf1e6
Dark Soul Studios <113238820+Dark-Soul9@users.noreply.github.com>
2024-07-15 23:14:05 +0500

    Programming (#12)
    * Added test enemy and patrol script
    
    * Updated samplescene
    
    * Added projectile game object
    
    * Added projectile fire logic
    
    * Added Enemies Layer
    
    * Set Projectile collider to ignore enemy layer
    
    * Added Diagonal firing
    
    * Projectile damage (#7)
    
    * Added damage to destructible objects
    
    * Fixed projectile collision behavior
    
    * Removed Debug.Log statement
    
    * Enemy behaviour (#8)
    
    * Enemy
    
    Defined enemy states
    
    * enemy states update
    
    -added Debug statements to switch between states
    -Patrolling, Following, Attacking functions created
    -bool variable added to determine whether it will follow or not.
    
    * minor bug fixes
    
    -added some variables to help with the logic
    -state changing now working
    
    * Dynamic platform generator (#11)
    
    * Added Narrative Bubble Assets and Script
    
    Added Narrative Bubble Assets and Script
    
    * Added Start Menu and assets
    
    Added Start Menu and assets
    
    * Updates
    
    * Naming update
    
    * Build export
    
    * Added Platform Generator and Behavior
    
    * Scene sync
    
    * Adjusted PlatformGenerator And PlatformBehavoir
    
    * Scene Sync
    
    * Adjusted platform generation timing
    
    * Additional testing adjustments
    
    * Art assets and animations
    
    Implemented character animations into the game.
    
    Added more art assets into the game
    - Water
    - Characters
    - Cloud block
    - Skybox
    
    * Squashed commit of the following:
    
    commit 2db405fbc5d892309e109b61a2b676832a20a16c
    Author: Charles Haitkin <chaitkin@gmail.com>
    Date:   Mon Jul 15 01:12:29 2024 -0700
    
        Update _GameLevel-2.unity
    
    commit 8c0bacc3c6fb6461b8153835eef3f12eb8fe5bed
    Author: Charles Haitkin <chaitkin@gmail.com>
    Date:   Mon Jul 15 00:33:16 2024 -0700
    
        Added white narrative bubble
    
    commit 1eeb6d0dc28ea9b6fc7ceebcafc14b6e3a2e9054
    Author: Charles Haitkin <chaitkin@gmail.com>
    Date:   Sun Jul 14 21:18:53 2024 -0700
    
        Updating Sanctuary and creating narrative bubbles
    
    commit 5b0cdb2d51e9fd6e00b8ed3dc19a667ec4ddc752
    Author: Charles Haitkin <chaitkin@gmail.com>
    Date:   Sun Jul 14 19:41:58 2024 -0700
    
        Added Sanctuary and level Blocks
    
    commit e7faa3d0f672ba0300b1dd0588be93048036eb2d
    Author: Charles Haitkin <chaitkin@gmail.com>
    Date:   Sun Jul 14 15:20:38 2024 -0700
    
        Update _GameLevel-1.unity
    
        Started creating level 1 before it was suggested to make it its own branch
    
    * Corrected Platform behavior
    
    * Rebuilt platform generator
    
    * Scene sync
    
    * Increased Jump Force
    
    ---------
    
    Co-authored-by: Charles Haitkin <chaitkin@gmail.com>
    Co-authored-by: bisaiah19 <bisaiahmeeks@gmail.com>
    
    ---------
    
    Co-authored-by: Dee Jae Mann <dee.j.mann@hotmail.com>
    Co-authored-by: Dee Jae Mann <147277261+DeeJaeMann@users.noreply.github.com>
    Co-authored-by: Charles Haitkin <chaitkin@gmail.com>
    Co-authored-by: bisaiah19 <bisaiahmeeks@gmail.com>

---
+++ Assets/Scripts/PlatformBehavior.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.RestService;
+using UnityEngine;
+
+public class PlatformBehavior : MonoBehaviour
+{
+    //public float platformLife = 30;
+    //public float speed = 2;
+    GameObject thisPlatform;
+    Rigidbody2D thisRigidbody;
+    PlatformGenerator platformGenerator;
+
+    //float platformLifeLeft;
+    public Vector2 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisPlatform = this.gameObject;
+        thisRigidbody = GetComponent<Rigidbody2D>();
+        platformGenerator = GameObject.FindObjectOfType<PlatformGenerator>();
+        velocity = new Vector2(0f, 1.1f);
+        //velocity = new Vector2(thisPlatform.transform.position.x, thisPlatform.transform.position.y + .1f); 
+
+        //thisRigidbody.AddForce(Vector2.up, ForceMode2D.Impulse);
+        //thisRigidbody.velocity = Vector3.up;
+        //thisRigidbody.velocity *= speed;
+
+        //platformLifeLeft = platformLife;
+
+        //Destroy(thisPlatform, platformLife);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        platformLifeLeft -= Time.deltaTime;
+
+        if (platformLifeLeft <= 0)
+        {
+            Die();
+        }
+        */
+    }
+
+    private void FixedUpdate()
+    {
+        thisRigidbody.MovePosition(thisRigidbody.position + velocity * Time.fixedDeltaTime);
+
+        // Check if this platform has reached the max height for the level
+        if( thisRigidbody.position.y >= platformGenerator.endLevelHeight )
+        {
+            Die();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log($"I hit something in CollisionEnter {collision.gameObject.name} at {collision.gameObject.transform.position}");
+        if (collision.gameObject.tag != "Player" && collision.gameObject.tag != "Projectile")
+        {
+            //Debug.Log($"{thisPlatform} hit {collision.gameObject.name}");
+            //Debug.Log("It's not a player!");
+            Die();
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log($"I hit something in CollisionStay {collision.gameObject.name}");
+        if (collision.gameObject.tag != "Player" && collision.gameObject.tag != "Projectile")
+        {
+            //Debug.Log($"{thisPlatform} hit {collision.gameObject.name}");
+            //Debug.Log("It's not a player!");
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+            Destroy(thisPlatform);
+            //platformGenerator.currentPlatforms++;
+
+    }
+}
