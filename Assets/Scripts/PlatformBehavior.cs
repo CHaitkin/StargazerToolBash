f80907d7a0d96cba56ed1f3701b0577d44e93594
Charles Haitkin <chaitkin@gmail.com>
2024-07-18 15:27:13 -0700

    Added narrative bubbles

--- Assets/Scripts/PlatformBehavior.cs
+++ Assets/Scripts/PlatformBehavior.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.RestService;
+//using UnityEditor.Experimental.RestService;
 using UnityEngine;
 
 public class PlatformBehavior : MonoBehaviour
